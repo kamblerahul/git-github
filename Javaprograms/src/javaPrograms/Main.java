4c32f74dba746f4c29036fad48811f8a0c178afd
kamblerahul <rahul.kamble1093@gmail.com>
2020-07-23 13:26:07 +0530

    java programs

---
+++ Javaprograms/src/javaPrograms/Main.java
@@ -0,0 +1,12 @@
+package javaPrograms;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello world");
+		System.out.println("Hello world 2");
+
+	}
+
+}
