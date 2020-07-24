5def624858090134885b882cb8ae2853077a195a
kamblerahul <rahul.kamble1093@gmail.com>
2020-07-24 18:00:15 +0530

    commited

---
+++ LearningPhase/src/com/LearningPhase/ExplicitCobversion.java
@@ -0,0 +1,14 @@
+package com.LearningPhase;
+
+public class ExplicitCobversion {
+
+	public static void main(String[] args) 
+	{
+		long l = 45l;
+		double d = l;
+		
+		System.out.println(d);
+
+	}
+
+}
